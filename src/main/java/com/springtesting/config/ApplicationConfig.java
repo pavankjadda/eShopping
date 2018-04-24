de5973bc0a15d0eeca9e8c4a75699d1b7271d8ee
pjadda <jpavanaryan@gmail.com>
2018-04-23 21:55:02 -0400

    Refactoered Code

---
+++ src/main/java/com/springtesting/config/ApplicationConfig.java
@@ -0,0 +1,40 @@
+package com.springtesting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SuppressWarnings("deprecation")
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"com.springtesting"})
+public class ApplicationConfig  extends WebMvcConfigurerAdapter
+{
+    public ApplicationConfig()
+    {
+        super();
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry)
+    {
+        super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
