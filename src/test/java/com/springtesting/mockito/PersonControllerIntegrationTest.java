2423e1fbe5da52b8fc5e84860cc499f5677bd485
pavankjaddabcmc <Bcmc@1218>
2018-10-29 11:49:39 -0400

    Updated pom.xml and IntegrationTest file

--- src/test/java/com/springtesting/mockito/PersonControllerIntegrationTest.java
+++ src/test/java/com/springtesting/mockito/PersonControllerIntegrationTest.java
@@ -13,14 +13,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.is;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringtestingApplication.class)
@@ -41,7 +44,7 @@ public class PersonControllerIntegrationTest
         Person jason = createPerson("1002", "Jason", "Stan", "john@gms.cim", "333333");
         Person jack = createPerson("1003", "Jack", "Reacher", "john@gms.cim", "333333");
 
-        mockMvc.perform(get("/api/persons").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)).andExpect(jsonPath("$[0].id", is(john.getId())));
+        mockMvc.perform(get("/api/persons").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)).andExpect((ResultMatcher) jsonPath("$[0].id", is(john.getId())));
     }
 
 
