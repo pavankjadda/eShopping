afda28930c62d1e1482f761d49dd0f885e61cd9b
OpenSource-Developer <testuser3130@outlook.com>
2018-11-07 21:55:59 -0500

    Login Validation added

---
+++ src/main/java/com/springtesting/security/ValidateUserSession.java
@@ -0,0 +1,8 @@
+package com.springtesting.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidateUserSession
+{
+}
--- src/main/java/com/springtesting/web/LoginController.java
+++ src/main/java/com/springtesting/web/LoginController.java
@@ -1,11 +1,15 @@
 package com.springtesting.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,28 +17,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController
 {
+    private Logger log=LoggerFactory.getLogger(LoginController.class);
+
     @Resource(name="authenticationManager")
     private AuthenticationManager authManager;
 
     @GetMapping(value = {"/","/login"})
-    public ModelAndView getLoginPage()
+    public ModelAndView getLoginPage(HttpServletRequest request)
     {
-
         ModelAndView modelAndView=new ModelAndView();
-         /*
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        if(securityContext.getAuthentication().isAuthenticated() )
-        {
-            modelAndView.setViewName("home");
-            return modelAndView;
-        }*/
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        // If NOT anonymous user, get user info
+        if (!(authentication instanceof AnonymousAuthenticationToken))
+        {
+            String currentUserName = authentication.getName();
+            String username=request.getUserPrincipal().getName();
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken= (UsernamePasswordAuthenticationToken) request.getUserPrincipal();
+            //usernamePasswordAuthenticationToken.getPrincipal().toString();
+            PreAuthenticatedAuthenticationToken preAuthenticatedAuthenticationToken= (PreAuthenticatedAuthenticationToken) request.getUserPrincipal();
+            preAuthenticatedAuthenticationToken.getPrincipal().toString();
+            Cookie[] cookies=request.getCookies();
+            for(Cookie cookie: cookies)
+            {
+                if(cookie.getName().equals("JSESSIONID"))
+                {
+                    String sessionValue=cookie.getValue();
+                }
+            }
+            modelAndView.setViewName("redirect:home");
+            return modelAndView;
+        }
+        else
+        {
+            modelAndView.addObject("message", "Login Page");
+            modelAndView.setViewName("login");
+        }
         modelAndView.addObject("message", "Login Page");
         modelAndView.setViewName("login");
         return modelAndView;
@@ -44,23 +69,31 @@ public class LoginController
     @PostMapping(value = "/login")
     public ModelAndView validateLoginDetails(@RequestParam("username") String username, @RequestParam("password") String password, final HttpServletRequest request)
     {
+        /*
         //Store Session
         UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(username, password);
         Authentication auth = authManager.authenticate(authReq);
+        //PreAuthenticatedAuthenticationToken preAuthenticatedAuthenticationToken=new PreAuthenticatedAuthenticationToken();
         SecurityContext sc = SecurityContextHolder.getContext();
         sc.setAuthentication(auth);
         HttpSession session = request.getSession(true);
         session.setAttribute("SPRING_SECURITY_CONTEXT", sc);
-
+*/
         ModelAndView modelAndView=new ModelAndView();
         modelAndView.setViewName("home");
         return modelAndView;
     }
 
     @GetMapping(value = {"/home","/home.html"})
-    public ModelAndView loadHomePage()
+    public ModelAndView loadHomePage(HttpServletRequest request)
     {
         ModelAndView modelAndView=new ModelAndView();
+        Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+        if(authentication instanceof AnonymousAuthenticationToken)
+        {
+            modelAndView.setViewName("redirect:login");
+            return modelAndView;
+        }
         modelAndView.setViewName("home");
         return modelAndView;
     }
