e0a08f570c47d050a86387e6495436934b022cb3
OpenSource-Developer <testuser3130@outlook.com>
2018-11-10 00:32:41 -0500

    EOD Commit 11/09/2018

--- src/main/java/com/springtesting/SpringtestingApplication.java
+++ src/main/java/com/springtesting/SpringtestingApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringtestingApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(SpringtestingApplication.class, args);
