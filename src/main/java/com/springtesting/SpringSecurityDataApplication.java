02c0a5650b6485ffd18c57aeafa249788549a350
Pavan Jadda <jpavanaryan@gmail.com>
2018-12-07 15:53:56 -0500

    Added MVC Code

--- src/main/java/com/springtesting/SpringSecurityDataApplication.java
+++ src/main/java/com/springtesting/SpringSecurityDataApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringtestingApplication
+public class SpringSecurityDataApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(SpringtestingApplication.class, args);
+        SpringApplication.run(SpringSecurityDataApplication.class, args);
     }
 }
 
