fa76f2a44926669553b7bcaaa6aaf4227d6dc1d9
Pavan Jadda <jpavanaryan@gmail.com>
2018-12-14 18:06:57 -0500

    AopConfig file will be removed in future release

--- src/main/java/com/springtesting/repo/UserRepository.java
+++ src/main/java/com/springtesting/repo/UserRepository.java
@@ -2,12 +2,8 @@ package com.springtesting.repo;
 
 import com.springtesting.model.User;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Long>
 {
--- src/test/java/com/springtesting/mockito/SecurityDataTest.java
+++ src/test/java/com/springtesting/mockito/SecurityDataTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
