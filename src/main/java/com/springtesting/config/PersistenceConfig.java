b2db238bfd7f50991cddbe4815d0a16f8bfa9578
Pavan Jadda <jpavanaryan@gmail.com>
2018-12-23 02:44:19 -0500

    AbstractAuditingEntity class added with Audit Support

--- src/main/java/com/springtesting/config/PersistenceConfig.java
+++ src/main/java/com/springtesting/config/PersistenceConfig.java
@@ -1,10 +1,18 @@
 package com.springtesting.config;
 
 
+import com.springtesting.audit.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class PersistenceConfig
 {
+    @Bean
+    AuditorAware<String> auditorProvider()
+    {
+        return new AuditorAwareImpl();
+    }
 }
