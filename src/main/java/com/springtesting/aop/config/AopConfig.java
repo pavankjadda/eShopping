b1724896ba77b9db465b2c3183ef66eebd2bc600
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-06 17:50:59 -0500

    AOP Config Updated

--- src/main/java/com/springtesting/aop/config/AopConfig.java
+++ src/main/java/com/springtesting/aop/config/AopConfig.java
@@ -1,5 +1,7 @@
 package com.springtesting.aop.config;
 
+import com.springtesting.aop.security.AuthenticationSuccessLogger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -7,4 +9,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AopConfig
 {
+
+    @Bean
+    public AuthenticationSuccessLogger loggingAspect()
+    {
+        return new AuthenticationSuccessLogger();
+    }
 }
--- src/main/java/com/springtesting/aop/security/AuthenticationSuccessLogger.java
+++ src/main/java/com/springtesting/aop/security/AuthenticationSuccessLogger.java
@@ -1,24 +1,77 @@
 package com.springtesting.aop.security;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @Aspect
-@Component
 public class AuthenticationSuccessLogger
 {
     private Logger logger=LoggerFactory.getLogger(AuthenticationSuccessLogger.class);
 
+
+
+    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
+            " || within(@org.springframework.stereotype.Service *)" +
+            " || within(@org.springframework.web.bind.annotation.RestController *)")
+    public void springBeanPointcut()
+    {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    /**
+     * Pointcut that matches all Spring beans in the application's main packages.
+     */
+    @Pointcut("within(com.springtesting.repo..*)"+
+            " || within(com.springtesting.service..*)"+
+            " || within(com.springtesting.web..*) || within(com.springtesting.security.handlers.CustomAuthenticationSuccessHandler..*)")
+    public void applicationPackagePointcut()
+    {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+
+    @Around("applicationPackagePointcut() && springBeanPointcut()")
+    public Object logCustomAuthenticationSuccessHandlerPointcut(ProceedingJoinPoint proceedingJoinPoint)
+    {
+        logger.info("Enter: {}.{}() with argument[s] = {}", proceedingJoinPoint.getSignature().getDeclaringTypeName(),
+                proceedingJoinPoint.getSignature().getName(), Arrays.toString(proceedingJoinPoint.getArgs()));
+
+        Object result = null;
+        try
+        {
+            result = proceedingJoinPoint.proceed();
+            System.out.println("proceedingJoinPoint.proceed() result: " + result);
+        }
+        catch (Throwable throwable)
+        {
+            throwable.printStackTrace();
+        }
+
+        logger.info("Exit: {}.{}() with result = {}", proceedingJoinPoint.getSignature().getDeclaringTypeName(),
+                proceedingJoinPoint.getSignature().getName(), result);
+        return result;
+    }
+
+    /**
+     * Advice that logs methods throwing exceptions.
+     *
+     * @param joinPoint join point for advice
+     * @param e exception
+     */
+    @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e)
+    {
+            logger.info("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+
+    }
+
+       /*
     @Pointcut(value = "execution(* com.springtesting.security.handlers.CustomAuthenticationSuccessHandler.onAuthenticationSuccess(..)) && args(request,response,authentication,..)",
             argNames = "request,response,authentication")
     private void getOnAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {};
@@ -48,4 +101,9 @@ public class AuthenticationSuccessLogger
         logger.error("Inside: AuthenticationSuccessLogger afterCustomAuthenticationSuccessHandler()");
     }
 
+
+    @Pointcut(value = "execution(* com.springtesting.security.handlers.CustomAuthenticationSuccessHandler.*(..))")
+    private void customAuthenticationSuccessHandlerPointcut() {};
+
+    */
 }
