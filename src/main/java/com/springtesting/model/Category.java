fd4638d4b908e5dc65d0670fba74dc198f6b4d9a
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-06 21:10:45 -0500

    Pointcut expressions updated

--- src/main/java/com/springtesting/model/Category.java
+++ src/main/java/com/springtesting/model/Category.java
@@ -2,10 +2,7 @@ package com.springtesting.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -17,7 +14,8 @@ public class Category extends AbstractAuditingEntity
 
     @Id
     @Column(name = "id")
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @NotNull(message = "Category name must not be null")
     @NotEmpty
@@ -29,7 +27,7 @@ public class Category extends AbstractAuditingEntity
 
     }
 
-    public Category(String id,  String name)
+    public Category(Long id,  String name)
     {
         this.id = id;
         this.name = name;
