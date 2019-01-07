1a1e7372664be1fca366517bc1c0865ab449f4ab
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-06 19:20:07 -0500

    JPA Auditing will be disabled in future commit as it inherits the same flaws of the JPA approach, so the delete operation cannot be audited.

--- src/main/java/com/springtesting/config/PersistenceConfig.java
+++ src/main/java/com/springtesting/config/PersistenceConfig.java
@@ -3,10 +3,11 @@ package com.springtesting.config;
 
 import com.springtesting.audit.AuditorAwareImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class PersistenceConfig
 {
