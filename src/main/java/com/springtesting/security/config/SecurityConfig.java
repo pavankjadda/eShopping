03c81df0b5ac1d98b27015d32afaa71fb7779641
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-08 22:01:23 -0500

    Before Removing SessionHistory Implementation. Will be added in future

--- src/main/java/com/springtesting/security/config/SecurityConfig.java
+++ src/main/java/com/springtesting/security/config/SecurityConfig.java
@@ -25,7 +25,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 
--- src/main/java/com/springtesting/web/SessionRegistryImplController.java
+++ src/main/java/com/springtesting/web/SessionRegistryImplController.java
@@ -1,17 +1,7 @@
 package com.springtesting.web;
 
-import com.springtesting.security.MyUserDetails;
-import com.springtesting.security.MyUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.session.SessionInformation;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/admin/session-registry")
