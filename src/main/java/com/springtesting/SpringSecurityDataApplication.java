fa169d26e5f12113c6ba6f9e8db3afa3792996c7
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-24 00:33:42 -0500

    Added Authentication Entry Point

--- src/main/java/com/springtesting/SpringSecurityDataApplication.java
+++ src/main/java/com/springtesting/SpringSecurityDataApplication.java
@@ -10,7 +10,5 @@ public class SpringSecurityDataApplication
     {
         SpringApplication.run(SpringSecurityDataApplication.class, args);
     }
-
-
 }
 
---
+++ src/main/java/com/springtesting/exceptions/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.springtesting.exceptions;
+
+/*
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler
+{
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<Object> handleAccessDeniedException(Exception exception, WebRequest webRequest)
+    {
+        return new ResponseEntity<>("Authentication Failed", new HttpHeaders(), HttpStatus.UNAUTHORIZED);
+    }
+}
+*/
+
