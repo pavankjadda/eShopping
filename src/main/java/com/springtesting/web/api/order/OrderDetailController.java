d66c420c7f4123d2102418b7d7511c9e86b8dc38
Pavan Jadda <jpavanaryan@gmail.com>
2019-04-15 12:52:45 -0400

    Order Detail controller updated

--- src/main/java/com/springtesting/web/api/order/OrderDetailController.java
+++ src/main/java/com/springtesting/web/api/order/OrderDetailController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/orderdetail")
+@RequestMapping("/api/v1/order_detail")
 public class OrderDetailController
 {
     private OrderDetailRepository orderDetailRepository;
