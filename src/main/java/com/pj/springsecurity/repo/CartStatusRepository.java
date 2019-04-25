9c01285263ff8cf4b1d73808721676fecc15ecc3
Pavan Jadda <jpavanaryan@gmail.com>
2019-04-25 18:06:12 -0400

    Updated Cart DTO and Controller classes

--- src/main/java/com/pj/springsecurity/repo/CartStatusRepository.java
+++ src/main/java/com/pj/springsecurity/repo/CartStatusRepository.java
@@ -3,6 +3,9 @@ package com.pj.springsecurity.repo;
 import com.pj.springsecurity.model.cart.CartStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartStatusRepository extends JpaRepository<CartStatus,Long>
 {
+    Optional<CartStatus> findByStatus(String status);
 }
--- src/main/java/com/pj/springsecurity/web/api/cart/CartController.java
+++ src/main/java/com/pj/springsecurity/web/api/cart/CartController.java
@@ -7,6 +7,7 @@ import com.pj.springsecurity.model.cart.CartProduct;
 import com.pj.springsecurity.model.user.UserProfile;
 import com.pj.springsecurity.repo.CartProductRepository;
 import com.pj.springsecurity.repo.CartRepository;
+import com.pj.springsecurity.repo.CartStatusRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,14 +19,16 @@ public class CartController
 {
     private final CartRepository cartRepository;
     private final CartProductRepository cartProductRepository;
+    private final CartStatusRepository cartStatusRepository;
 
     private final ModelMapper modelMapper;
 
-    public CartController(CartRepository cartRepository, ModelMapper modelMapper, CartProductRepository cartProductRepository)
+    public CartController(CartRepository cartRepository, ModelMapper modelMapper, CartProductRepository cartProductRepository, CartStatusRepository cartStatusRepository)
     {
         this.cartRepository = cartRepository;
         this.modelMapper = modelMapper;
         this.cartProductRepository = cartProductRepository;
+        this.cartStatusRepository = cartStatusRepository;
     }
 
     @GetMapping(path = "/find/user/{id}")
@@ -41,6 +44,7 @@ public class CartController
 
         Cart cart=new Cart();
         cart.setUserProfile(userProfile);
+        cart.setCartStatus(cartStatusRepository.findByStatus("Draft").orElse(null));
         return cartRepository.saveAndFlush(cart);
     }
 
