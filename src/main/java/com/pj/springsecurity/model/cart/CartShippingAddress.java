27a763f5637b847330330326480170307b12f5bb
Pavan Jadda <jpavanaryan@gmail.com>
2019-05-06 18:21:37 -0400

    Added Shipping Address and Billing Address model classes

---
+++ src/main/java/com/pj/springsecurity/model/cart/CartShippingAddress.java
@@ -0,0 +1,58 @@
+package com.pj.springsecurity.model.cart;
+
+import com.pj.springsecurity.audit.AbstractAuditingEntity;
+import com.pj.springsecurity.model.user.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Table(name = "cart_shipping_address")
+public class CartShippingAddress extends AbstractAuditingEntity
+{
+
+    private static final long serialVersionUID = 5015149636428611585L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street_name")
+    private String streetName;
+
+    @Column(name = "apartment")
+    private String apartment;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @ManyToOne
+    @JoinColumn(name = "state_id")
+    private State state;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @ManyToOne
+    @JoinColumn(name = "address_type_id", referencedColumnName = "id")
+    private AddressType addressType;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+}
