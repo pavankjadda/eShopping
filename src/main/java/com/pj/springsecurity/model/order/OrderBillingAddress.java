0f6fe4a1758ad8cab5c1ac7bb3538c2377cfe53a
Pavan Jadda <jpavanaryan@gmail.com>
2019-05-06 17:58:48 -0400

    Added Shipping Address and Billing Address model classes

---
+++ src/main/java/com/pj/springsecurity/model/order/OrderBillingAddress.java
@@ -0,0 +1,51 @@
+package com.pj.springsecurity.model.order;
+
+import com.pj.springsecurity.audit.AbstractAuditingEntity;
+import com.pj.springsecurity.model.user.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Table(name = "order_billing_address")
+public class OrderBillingAddress extends AbstractAuditingEntity
+{
+    private static final long serialVersionUID = -670201478701113253L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street_name")
+    private String streetName;
+
+    @Column(name = "apartment")
+    private String apartment;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @ManyToOne
+    @JoinColumn(name = "state_id")
+    private State state;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @ManyToOne
+    @JoinColumn(name = "address_type_id", referencedColumnName = "id")
+    private AddressType addressType;
+}
---
+++ src/main/java/com/pj/springsecurity/model/order/OrderShippingAddress.java
@@ -0,0 +1,52 @@
+package com.pj.springsecurity.model.order;
+
+
+import com.pj.springsecurity.audit.AbstractAuditingEntity;
+import com.pj.springsecurity.model.user.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Table(name = "order_shipping_address")
+public class OrderShippingAddress extends AbstractAuditingEntity
+{
+    private static final long serialVersionUID = 8733009552741603083L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street_name")
+    private String streetName;
+
+    @Column(name = "apartment")
+    private String apartment;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @ManyToOne
+    @JoinColumn(name = "state_id")
+    private State state;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @ManyToOne
+    @JoinColumn(name = "address_type_id", referencedColumnName = "id")
+    private AddressType addressType;
+}
