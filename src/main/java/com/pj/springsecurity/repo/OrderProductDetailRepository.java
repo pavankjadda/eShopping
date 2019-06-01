e025754219e1b0f7c2fec346f6fe61c4898425a6
Pavan Jadda <jpavanaryan@gmail.com>
2019-06-01 04:15:40 -0400

    Create order in progress

---
+++ src/main/java/com/pj/springsecurity/repo/OrderProductDetailRepository.java
@@ -0,0 +1,8 @@
+package com.pj.springsecurity.repo;
+
+import com.pj.springsecurity.model.order.OrderProductDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderProductDetailRepository extends JpaRepository<OrderProductDetail,Long>
+{
+}
--- src/main/java/com/pj/springsecurity/web/api/order/OrderController.java
+++ src/main/java/com/pj/springsecurity/web/api/order/OrderController.java
@@ -3,15 +3,17 @@ package com.pj.springsecurity.web.api.order;
 import com.pj.springsecurity.model.cart.Cart;
 import com.pj.springsecurity.model.cart.CartProduct;
 import com.pj.springsecurity.model.order.Order;
+import com.pj.springsecurity.model.order.OrderBillingAddress;
+import com.pj.springsecurity.model.order.OrderProductDetail;
+import com.pj.springsecurity.model.order.OrderShippingAddress;
 import com.pj.springsecurity.model.tax.TaxRate;
-import com.pj.springsecurity.repo.CartRepository;
-import com.pj.springsecurity.repo.OrderRepository;
-import com.pj.springsecurity.repo.OrderStatusRepository;
-import com.pj.springsecurity.repo.TaxRateRepository;
+import com.pj.springsecurity.repo.*;
 import com.pj.springsecurity.util.UserInfoUtil;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,20 +25,26 @@ public class OrderController
 
     private final OrderStatusRepository orderStatusRepository;
 
+    private final OrderProductDetailRepository orderProductDetailRepository;
+
     private final UserInfoUtil userInfoUtil;
 
     private final CartRepository cartRepository;
 
     private final TaxRateRepository taxRateRepository;
 
+    private final ModelMapper modelMapper;
 
-    public OrderController(OrderRepository orderRepository, OrderStatusRepository orderStatusRepository, UserInfoUtil userInfoUtil, CartRepository cartRepository, TaxRateRepository taxRateRepository)
+
+    public OrderController(OrderRepository orderRepository, OrderStatusRepository orderStatusRepository, OrderProductDetailRepository orderProductDetailRepository, UserInfoUtil userInfoUtil, CartRepository cartRepository, TaxRateRepository taxRateRepository, ModelMapper modelMapper)
     {
         this.orderRepository = orderRepository;
         this.orderStatusRepository = orderStatusRepository;
+        this.orderProductDetailRepository = orderProductDetailRepository;
         this.userInfoUtil = userInfoUtil;
         this.cartRepository = cartRepository;
         this.taxRateRepository = taxRateRepository;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping(value = "/list")
@@ -66,10 +74,40 @@ public class OrderController
             order.setOrderStatus(orderStatusRepository.findByStatus("Created"));
             order.setShippingCharge((double) 0);
             order.setOrderCreatedDateTime(LocalDateTime.now());
+            OrderShippingAddress orderShippingAddress=modelMapper.map(cart.getCartShippingAddress(),OrderShippingAddress.class);
+            orderShippingAddress.setId(null);
+            orderShippingAddress.setOrder(order);
 
+            OrderBillingAddress orderBillingAddress=modelMapper.map(cart.getCartBillingAddress(),OrderBillingAddress.class);
+            orderBillingAddress.setId(null);
+            orderBillingAddress.setOrder(order);
+
+            order.setOrderShippingAddress(orderShippingAddress);
+            order.setOrderBillingAddress(orderBillingAddress);
+
+            order.setOrderProductDetails(copyCartProducts(cart));
             return orderRepository.saveAndFlush(order);
         }
         return null;
+    }
+
+    /*
+       Copy cart products to Order
+     */
+    private List<OrderProductDetail> copyCartProducts(Cart cart)
+    {
+        List<OrderProductDetail> orderProductDetailList=new ArrayList<>();
+        List<CartProduct> cartProductList=cart.getCartProducts();
+        for(CartProduct cartProduct: cartProductList)
+        {
+            OrderProductDetail orderProductDetail=new OrderProductDetail();
+            orderProductDetail.setProduct(cartProduct.getProduct());
+            orderProductDetail.setQuantity(cartProduct.getQuantity());
+            orderProductDetail.setOriginalPrice(cartProduct.getProduct().getPrice().getAmount());
+            orderProductDetail=orderProductDetailRepository.saveAndFlush(orderProductDetail);
+            orderProductDetailList.add(orderProductDetail);
+        }
+        return orderProductDetailList;
     }
 
     private Double calculateTotalCostBeforeTax(Cart cart)
