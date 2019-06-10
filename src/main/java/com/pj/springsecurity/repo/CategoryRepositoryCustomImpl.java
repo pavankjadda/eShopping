b78384aeec8b6bd1a28ecd9d8a2d3ac9b2ecb850
Pavan Jadda <>
2019-06-10 16:12:16 -0400

    Added Entity Manager for Stored Procedure

--- src/main/java/com/pj/springsecurity/repo/CategoryRepositoryCustomImpl.java
+++ src/main/java/com/pj/springsecurity/repo/CategoryRepositoryCustomImpl.java
@@ -1,12 +1,15 @@
 package com.pj.springsecurity.repo;
 
 import com.pj.springsecurity.model.category.Category;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.StoredProcedureQuery;
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Repository
 public class CategoryRepositoryCustomImpl implements CategoryRepositoryCustom
@@ -14,9 +17,20 @@ public class CategoryRepositoryCustomImpl implements CategoryRepositoryCustom
     @PersistenceContext
     private EntityManager entityManager;
 
+    private final ModelMapper modelMapper;
+
+    public CategoryRepositoryCustomImpl(ModelMapper modelMapper)
+    {
+        this.modelMapper = modelMapper;
+    }
+
     public List<Category> getAllCategoriesThroughStoredProcedureCustom()
     {
+        List<Category> categories=new ArrayList<>();
         StoredProcedureQuery storedProcedureQuery=entityManager.createNamedStoredProcedureQuery("getAllCategoriesThroughStoredProcedureCustom");
-        return storedProcedureQuery.getResultList();
+        List<?> categoriesResult=storedProcedureQuery.getResultList();
+
+        categoriesResult.forEach(category -> categories.add(modelMapper.map(category, Category.class)));
+        return categories;
     }
 }
