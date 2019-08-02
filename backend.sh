77136ffd2eaf68fe2106b0a8ceca20cb14a3559f
Pavan K Jadda <17564080+pavankjadda@users.noreply.github.com>
2019-08-02 12:39:05 -0400

    Add automated script to build Angular app and run the program

--- backend.sh
+++ backend.sh
@@ -1,11 +1,6 @@
 # This script builds Angular app and moves the dist folder contents to resorces/static/ folder in Spring Boot app. Then maven builds the app and java command starts the combined app
 
 # shellcheck disable=SC2164
-cd /home/ubuntu/SpringSecurity-SpringData-UI
-git pull
-ng build --prod
-cp -rf dist/SpringTestingUI/* ../SpringSecurity-SpringData/src/main/resources/static/
-
 cd /home/ubuntu/SpringSecurity-SpringData
 git pull
 mvn clean package -DskipTests
--- frontend.sh
+++ frontend.sh
@@ -4,9 +4,3 @@
 cd /home/ubuntu/SpringSecurity-SpringData-UI
 git pull
 ng build --prod
-cp -rf dist/SpringTestingUI/* ../SpringSecurity-SpringData/src/main/resources/static/
-
-cd /home/ubuntu/SpringSecurity-SpringData
-git pull
-mvn clean package -DskipTests
-sudo java -jar -Dspring.profiles.active=prod target/SpringSecurity-SpringData-0.0.1-SNAPSHOT.jar
