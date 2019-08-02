aae4f43bff49a73ceb6acd1fafdf61c8af031050
Pavan K Jadda <17564080+pavankjadda@users.noreply.github.com>
2019-08-02 13:31:40 -0400

    Change hasAnyRole to hasAnyAuthority

--- src/main/java/com/pj/springsecurity/config/ApplicationConfig.java
+++ src/main/java/com/pj/springsecurity/config/ApplicationConfig.java
@@ -12,41 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer
 {
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
-            {
-                    "classpath:/META-INF/resources/", "classpath:/resources/"
-            };
-
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry)
-//    {
-//        registry.addResourceHandler("/**/*")
-//                .addResourceLocations("classpath:/static/")
-//                .resourceChain(true)
-//                .addResolver(new PathResourceResolver()
-//                {
-//                    @Override
-//                    protected Resource getResource(String resourcePath, Resource location) throws IOException
-//                    {
-//                        Resource requestedResource = location.createRelative(resourcePath);
-//                        return requestedResource.exists() && requestedResource.isReadable() ?
-//                                requestedResource : new ClassPathResource("/static/index.html");
-//                    }
-//                });
-//    }
-
-   /* @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry)
-    {
-        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry)
-    {
-        registry.addViewController("/").setViewName("/login.html");
-    }*/
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder()
     {
