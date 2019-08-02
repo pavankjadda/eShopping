72d0fcabfd5a492b8ed5b8d802b4656adc54b311
Pavan K Jadda <17564080+pavankjadda@users.noreply.github.com>
2019-08-02 13:29:25 -0400

    Change hasAnyRole to hasAnyAuthority

--- src/main/java/com/pj/springsecurity/config/ApplicationConfig.java
+++ src/main/java/com/pj/springsecurity/config/ApplicationConfig.java
@@ -3,17 +3,11 @@ package com.pj.springsecurity.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HttpSessionIdResolver;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
-
-import java.io.IOException;
 
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer
@@ -23,23 +17,23 @@ public class ApplicationConfig implements WebMvcConfigurer
                     "classpath:/META-INF/resources/", "classpath:/resources/"
             };
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry)
-    {
-        registry.addResourceHandler("/**/*")
-                .addResourceLocations("classpath:/static/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver()
-                {
-                    @Override
-                    protected Resource getResource(String resourcePath, Resource location) throws IOException
-                    {
-                        Resource requestedResource = location.createRelative(resourcePath);
-                        return requestedResource.exists() && requestedResource.isReadable() ?
-                                requestedResource : new ClassPathResource("/static/index.html");
-                    }
-                });
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry)
+//    {
+//        registry.addResourceHandler("/**/*")
+//                .addResourceLocations("classpath:/static/")
+//                .resourceChain(true)
+//                .addResolver(new PathResourceResolver()
+//                {
+//                    @Override
+//                    protected Resource getResource(String resourcePath, Resource location) throws IOException
+//                    {
+//                        Resource requestedResource = location.createRelative(resourcePath);
+//                        return requestedResource.exists() && requestedResource.isReadable() ?
+//                                requestedResource : new ClassPathResource("/static/index.html");
+//                    }
+//                });
+//    }
 
    /* @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry)
