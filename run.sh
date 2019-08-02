f5583e278871de2905ff138a58177c215512f6ba
Pavan K Jadda <17564080+pavankjadda@users.noreply.github.com>
2019-08-02 12:33:35 -0400

    Add automated script to build Angular app and run the program

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 cd /home/ubuntu/SpringSecurity-SpringData-UI
 ng build --prod
 cp -rf dist/SpringTestingUI/* ../SpringSecurity-SpringData/src/main/resources/static/
+cd /home/ubuntu/SpringSecurity-SpringData
 mvn clean package -DskipTests
 sudo java -jar -Dspring.profiles.active=prod target/SpringSecurity-SpringData-0.0.1-SNAPSHOT.jar
