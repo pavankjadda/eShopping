34118dd75700debb8ae6f7ed6e2e6d2f9c6fe2f0
Pavan K Jadda <17564080+pavankjadda@users.noreply.github.com>
2019-08-02 12:23:38 -0400

    Remove unused code

--- src/main/java/com/pj/springsecurity/web/api/user/LoginController.java
+++ src/main/java/com/pj/springsecurity/web/api/user/LoginController.java
@@ -11,7 +11,6 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,13 +32,6 @@ public class LoginController
 
     @GetMapping(value = {"/login"})
     public UserDto getUserLoginDetails(HttpServletRequest request)
-    {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return copyUser(authentication,request);
-    }
-
-    @PostMapping(value = {"/login"})
-    public UserDto postUserLoginDetails(HttpServletRequest request)
     {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return copyUser(authentication,request);
