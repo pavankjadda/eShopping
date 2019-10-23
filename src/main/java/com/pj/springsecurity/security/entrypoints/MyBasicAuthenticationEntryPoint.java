65c486a41dbd6b223f638c1facb914b5abda7ad2
pjadda <jpavanaryan@gmail.com>
2019-10-22 21:13:55 -0400

    Update DB config

--- src/main/java/com/pj/springsecurity/security/entrypoints/MyBasicAuthenticationEntryPoint.java
+++ src/main/java/com/pj/springsecurity/security/entrypoints/MyBasicAuthenticationEntryPoint.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,7 +14,7 @@ public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoi
 {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-            throws IOException, ServletException
+            throws IOException
     {
         response.addHeader("WWW-Authenticate", "Basic realm='' + getRealmName() + ''");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
