198536b68198e0f6e2848cd4ca86b1b737ea6a56
Pavan Jadda <jpavanaryan@gmail.com>
2020-09-01 22:11:53 -0400

    Upgrade to Spring Boot 2.3.3.RELEASE

--- src/test/java/com/pj/eshopping/consoleapplications/MainClass.java
+++ src/test/java/com/pj/eshopping/consoleapplications/MainClass.java
@@ -2,16 +2,15 @@ package com.pj.eshopping.consoleapplications;
 
 import com.pj.eshopping.domain.user.User;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class MainClass
 {
     public static void main(String[] args)
     {
-        User user = new User();
-        user.setActive(true);
-        user.setId(1L);
-        user.setPassword("admin");
-        user.setUsername("admin");
-
-        System.out.println(user.toString());
+        Set<User> userSet=List.of("one", "teo", "three").stream().map(s -> new User(s, s, true, null )).collect(Collectors.toSet());
+        userSet.forEach(user1 -> System.out.println(user1.toString()));
     }
 }
